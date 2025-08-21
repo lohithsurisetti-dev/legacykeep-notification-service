5e3e7700121fa99b83f9ec376b279af25f2fe1ad
lohith <lohith@psense.ai>
2025-08-21 00:32:13 -0500

    Initial commit: notification-service microservice

---
+++ src/main/java/com/legacykeep/service/UnotificationUserviceApplication.java
@@ -0,0 +1,12 @@
+package com.legacykeep.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnotificationUserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnotificationUserviceApplication.class, args);
+    }
+}
